58f4a9c504246c4b5397d08706e547292becd04e
Theodore Folkesson <theodore.folkesson@gmail.com>
2026-03-03 09:13:16 +0100

    fetch

--- OSRS/GE/geScript.js
+++ OSRS/GE/geScript.js
@@ -12,6 +12,19 @@ function updateShoppingList(items) {
     }
 }
 
+function updateItemList(items){
+    const itemList = document.getElementById("item-list");
+    if (items.length === 0) {
+        itemList.innerHTML = "<tr><td colspan='3'>No items found.</td></tr>";
+    } else {
+        items.forEach(item => {
+            const tr = document.createElement("tr");
+            tr.innerHTML = `<td>${item[0]}</td><td>${item[1].name}</td><td>${item[1].id}</td>`;
+            itemList.appendChild(tr);
+        });
+    }
+}
+
 function fetchGEData() {
     // Simulate fetching data from the Grand Exchange API
     fetch("https://api.weirdgloop.org/exchange/history/osrs/latest?name=Rune Scimitar|Air rune")
@@ -30,6 +43,23 @@ function fetchGEData() {
             console.error("Error fetching GE data:", error);
         });
 
+}
+
+function getEVERYitem() {
+    // Simulate fetching data from the Grand Exchange API
+    fetch("https://www.osrsbox.com/osrsbox-db/items-complete.json")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data); // Log the fetched data for debugging
+            items = [];
+            for (const item in data) {
+                items.push([item, data[item]]);
+            }
+            updateItemList(items);
+        })
+        .catch(error => {
+            console.error("Error fetching GE data:", error);
+        });
 }
 
 window.onload = function() {
